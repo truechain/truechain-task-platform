8698e71291d0b03b4388f5c3fbd3a9e7de6b58bb
xuhaohao <xuhaogogo@live.cn>
2018-08-12 23:05:23 +0800

    显示声明列表

--- admin/server/truechain-task-admin/src/main/java/com/truechain/task/admin/controller/ReportController.java
+++ admin/server/truechain-task-admin/src/main/java/com/truechain/task/admin/controller/ReportController.java
@@ -66,7 +66,7 @@ public class ReportController extends BasicController {
     @PostMapping("/index")
     public Wrapper index(@RequestHeader("Token") String token, @RequestHeader("Agent") String agent,@RequestBody TimeRangeDTO timeRange ){
         //ReportIndexPojo
-//        List<UserRewardHistoryPojo> rewardHistoryPojoList = Lists.newArrayList();
+
         ReportIndexPojo reportIndexPojo = new ReportIndexPojo();
         //通过审核用户数量
         long userCount = userService.countAuditPass(timeRange.getStartDate(),timeRange.getEndDate());
@@ -97,7 +97,8 @@ public class ReportController extends BasicController {
         //进行中任务数
         reportIndexPojo.setTaskDoingCount(reportIndexPojo.getTaskCount() - reportIndexPojo.getTaskDoneCount());
 
-        return WrapMapper.ok(Lists.newArrayList(reportIndexPojo));
+        List<ReportIndexPojo> rewardHistoryPojoList = Lists.newArrayList(reportIndexPojo);
+        return WrapMapper.ok(rewardHistoryPojoList);
     }
 
     /**
