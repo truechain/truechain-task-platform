9a08bc901cc604fdd284ff964ca230d88f9a2993
happyfromtbq <happyfromtbq@163.com>
2018-10-24 17:47:30 +0800

    sysuser count

--- api/truechain-task-api/src/main/java/com/truechain/task/api/controller/GlobalHandler.java
+++ api/truechain-task-api/src/main/java/com/truechain/task/api/controller/GlobalHandler.java
@@ -1,16 +1,18 @@
 package com.truechain.task.api.controller;
 
-import com.truechain.task.core.BusinessException;
-import com.truechain.task.core.NullException;
-import com.truechain.task.core.WrapMapper;
-import com.truechain.task.core.Wrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.truechain.task.core.BusinessException;
+import com.truechain.task.core.NullException;
+import com.truechain.task.core.WrapMapper;
+import com.truechain.task.core.Wrapper;
 
 @RestControllerAdvice
 public class GlobalHandler {
@@ -43,6 +45,12 @@ public class GlobalHandler {
     @ResponseBody
     public Wrapper exception(Exception e) {
         logger.info("全局异常信息{}", e.getMessage(), e);
+        if(e instanceof MissingServletRequestParameterException ){
+        	//请求参数异常
+        	MissingServletRequestParameterException me =  (MissingServletRequestParameterException)e;
+        	String msg = "请求参数:"+me.getParameterName()+"不存在";
+        	return WrapMapper.error(msg);
+        }
         return WrapMapper.error("系统异常");
     }
 
--- api/truechain-task-api/src/main/java/com/truechain/task/api/repository/SysUserRepository.java
+++ api/truechain-task-api/src/main/java/com/truechain/task/api/repository/SysUserRepository.java
@@ -1,10 +1,13 @@
 package com.truechain.task.api.repository;
 
-import com.truechain.task.model.entity.SysUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
-
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import com.truechain.task.model.entity.SysUser;
 
 public interface SysUserRepository extends JpaRepository<SysUser, Long>, QueryDslPredicateExecutor<SysUser> {
 
@@ -15,6 +18,9 @@ public interface SysUserRepository extends JpaRepository<SysUser, Long>, QueryDs
     SysUser findByMobile(String mobile);
 
     List<SysUser> findByRecommendUserId(long recommendUserId);
+    
+    @Query("select count(id) from SysUser where audit_status = 1 and recommend_user_id = :recommendUserId")
+    long countByRecommendUserIdAndAuditStatus(@Param("recommendUserId") long recommendUserId);
 
     long countByMobile(String mobile);
 }
