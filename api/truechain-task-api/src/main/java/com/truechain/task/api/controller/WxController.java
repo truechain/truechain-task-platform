a8fc02c8bfad361360849be4ebf759cfd3a699de
david208 <daviding2002@sina.com>
2018-10-30 17:32:27 +0800

    33

--- api/truechain-task-api/src/main/java/com/truechain/task/api/controller/WxController.java
+++ api/truechain-task-api/src/main/java/com/truechain/task/api/controller/WxController.java
@@ -3,12 +3,14 @@ package com.truechain.task.api.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.truechain.task.api.service.UserService;
 
 @RequestMapping("/wx")
+@Controller
 public class WxController {
 	
 	@Autowired
